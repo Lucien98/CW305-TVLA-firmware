2e3487c9fb1d3886a89adf2159de56562239892a
CryptoFGG <1137562038@qq.com>
2025-03-06 14:29:59 +0800

    add firmware that provivides encrypt 0 with key 0 for debug

--- software/chipwhisperer/update_fw.py
+++ software/chipwhisperer/update_fw.py
@@ -1,11 +1,22 @@
 import chipwhisperer as cw
 
-ORDER=2
 
-print(" You need first to short J5 and reset\n\n")
-fw_path = "../../hardware/victims/cw305_artixtarget/fw/sam3u/CW305_SAM3U_FW/build/CW305_SAM3UFWo%d.bin" % ORDER
-# fw_path = "../../hardware/victims/cw305_artixtarget/fw/sam3u/CW305_SAM3U_FW/build/CW305_SAM3UFW.bin"
-programmer = cw.SAMFWLoader(scope=None)
-programmer.program("COM7", fw_path)
-print("\n\n Reset the target again")
+def parse_arguments():
+	import argparse
 
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--order', metavar='order', required=False, default=2,
+							help='program the firmware according to the order', type=int)
+	args = parser.parse_args()
+	return args
+
+def main():
+	args = parse_arguments()
+	print(" You need first to short J5 and reset\n\n")
+	fw_path = "../../hardware/victims/cw305_artixtarget/fw/sam3u/CW305_SAM3U_FW/build/CW305_SAM3UFWo%d.bin" % args.order
+	programmer = cw.SAMFWLoader(scope=None)
+	programmer.program("COM7", fw_path)
+	print("\n\n Reset the target again")
+
+if __name__ == '__main__':
+	main()
